b07b56f9495f9d7f0236fdd8d4be2a5b4c71d05e
skipprrr <arifulabc420@gmail.com>
2026-07-13 19:53:55 +0600

    fix: standalone output for Vercel

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+}
+module.exports = nextConfig
